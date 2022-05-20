ef243c972d481a1bea936cfa42d39ad956d2e3eb
LeslieDiana <sepulvedaquispelesliediana@gmail.com>
2022-05-20 12:59:07 -0400

    tercer componente

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,13 +1,15 @@
 import '../styles/App.css';
 import Encabezado from './Encabezado';
 import Medio from './Medio';
+import Final from './Final';
 
 function App() {
   return (
     <>
       <Encabezado />
       <Medio />
-      </>
+      <Final />
+    </>
 
   );
 }
---
+++ src/componentes/Final.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Final() {
+  return (
+    <div>
+        <span>Last Updated</span>
+        <span>3/8/2020</span>
+    </div>
+  )
+}
+
+export default Final
