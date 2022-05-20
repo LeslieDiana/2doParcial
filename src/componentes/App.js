e8e352bb1d2f64726225d65485e1a8002b060743
LeslieDiana <sepulvedaquispelesliediana@gmail.com>
2022-05-20 12:53:20 -0400

    segundo componente

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,10 +1,13 @@
 import '../styles/App.css';
 import Encabezado from './Encabezado';
+import Medio from './Medio';
 
 function App() {
   return (
+    <>
       <Encabezado />
-
+      <Medio />
+      </>
 
   );
 }
---
+++ src/componentes/Medio.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Medio() {
+  return (
+    <div>Description of what is done here</div>
+  )
+}
+
+export default Medio
